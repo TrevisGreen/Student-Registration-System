2ad9649146223214f5a1a98b21f8d0f86d6513c5
TrevisGreen <Trevis.Green@gmail.com>
2023-04-05 14:12:41 -0500

    working on Admin class

--- src/Admin.java
+++ src/Admin.java
@@ -1,4 +1,8 @@
+import javax.swing.text.html.StyleSheet;
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Admin extends User {
@@ -37,6 +41,91 @@ public class Admin extends User {
     }
 
     public static void deleteCourse() {
+        // Ask for courseId
+        Course course = new Course();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Course Id: ");
+        String courseId = input.nextLine();
+
 
     }
+
+    public static void editCourse() throws IOException {
+        Course course = new Course();
+        Scanner input = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
+        System.out.print("Course Id: ");
+        String courseId = input.nextLine();
+        int courseIndex = 0;
+
+        // Ask user what they want to edit
+        System.out.print("What would you like to edit?\n");
+        System.out.print("1. Max amount of students\n");
+        System.out.print("2. Course Instructor\n");
+        System.out.print("3. Section Number\n");
+        System.out.print("4. Class location\n");
+        int choice = input.nextInt();
+
+
+    }
+
+    public static void display() throws IOException {
+        Course course = new Course();
+        Scanner input = new Scanner(System.in);
+
+        // Ask user for Course Id
+        System.out.print("Enter course Id: ");
+        String courseId = input.nextLine();
+
+
+    }
+
+    public static void registerStudent() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Student First Name: ");
+        String firstName = input.nextLine();
+        System.out.println("Student Last Name: ");
+        String lastName = input.nextLine();
+        String userName = null;
+        String password = null;
+
+
+    }
+
+    public static void viewCourses() {
+
+    }
+
+    public static String fullCourseId = null;
+    public static String viewFullCourses() throws IOException {
+        Course course = new Course();
+
+    }
+
+    public static void addCourseToFile() throws IOException {
+        Course course = new Course();
+
+    }
+
+    public static void coursesPerStudent() {
+        Course course = new Course();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter student first name: ");
+        String firstName = input.nextLine();
+
+        System.out.println("Enter student last name: ");
+        String lastName = input.nextLine();
+        String name = firstName = " " + lastName;
+        Student testStudent = new Student(firstName, lastName, null, null);
+        System.out.println("Their registered courses: ");
+
+
+    }
+
+    public static void sortMostStudentsPerClass() {
+        Course course = new Course();
+        ArrayList<Integer> sortedArrayList = new ArrayList<Integer>();
+
+    }
+
 }
