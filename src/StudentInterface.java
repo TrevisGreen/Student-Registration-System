6b6ce44c6b11cb073985160e0c44b4c8ec338afa
TrevisGreen <Trevis.Green@gmail.com>
2023-04-05 12:45:21 -0500

    Created Student Interface

---
+++ src/StudentInterface.java
@@ -0,0 +1,21 @@
+public interface StudentInterface {
+    static void viewAllCourses() {
+
+    };
+
+    static void viewFullCourses() {
+
+    };
+
+    static void register() {
+
+    };
+
+    static void withdraw() {
+
+    };
+
+    static void viewYourCourses() {
+
+    };
+}
