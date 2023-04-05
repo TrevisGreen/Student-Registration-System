95323266d64a8aa88ab355acddbad6d2b28fad2d
TrevisGreen <Trevis.Green@gmail.com>
2023-04-05 14:18:31 -0500

    Created Admin Interface

---
+++ src/AdminInterface.java
@@ -0,0 +1,44 @@
+public interface AdminInterface {
+    // Course Management
+    static void createCourse() {
+
+    };
+
+    static void deleteCourse() {
+
+    };
+
+    static void editCourse() {
+
+    };
+
+    static void displayCourse() {
+
+    };
+
+    static void registerStudent() {
+
+    };
+
+    // Reports
+    static void viewCourses() {
+
+    };
+
+    static void viewFullCourses() {
+
+    };
+
+    static void addCourseToFile() {
+
+    };
+
+    static void studentNamesInCourse() {
+
+    };
+
+    static void coursesPerStudent() {
+
+    };
+
+}
