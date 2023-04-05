bbfd4d77ebf822ccedc8aeca62cdfa5cb2fd682c
TrevisGreen <Trevis.Green@gmail.com>
2023-04-05 15:59:37 -0500

    Updated Admin class

--- src/Admin.java
+++ src/Admin.java
@@ -3,9 +3,10 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
-public class Admin extends User {
+public class Admin extends User implements AdminInterface, java.io.Serializable {
 
     // constructor with params
     public Admin(String fn, String ln, String usn, String psw) {
@@ -47,7 +48,12 @@ public class Admin extends User {
         System.out.print("Enter Course Id: ");
         String courseId = input.nextLine();
 
-
+        for(int i = 0; i < crsMain.courseArrayList.size(); i++) {
+            course = crsMain.courseArrayList.get(i);
+            if(course.getCourseId().equals(courseId)) {
+                crsMain.courseArrayList.remove(i);
+            }
+        }
     }
 
     public static void editCourse() throws IOException {
@@ -66,10 +72,40 @@ public class Admin extends User {
         System.out.print("4. Class location\n");
         int choice = input.nextInt();
 
+        for(int i = 0; i < crsMain.courseArrayList.size(); i++) {
+            course = crsMain.courseArrayList.get(i);
+            courseIndex = crsMain.courseArrayList.indexOf(crsMain.courseArrayList.get(i));
 
+            if(choice == 1 && courseId.equals(course.getCourseId())) {
+                System.out.print("New max amount of students: ");
+                int newMax = input2.nextInt();
+                crsMain.courseArrayList.get(courseIndex);
+                course.setMaxStudents(newMax);
+                break;
+            }
+            if(choice == 2 && courseId.equals(course.getCourseId())) {
+                System.out.println("New course instructor: ");
+                String newInstructor = input2.nextLine();
+                course.setCourseInstructor(newInstructor);
+                break;
+            }
+            if(choice == 3 && courseId.equals(course.getCourseId())) {
+                System.out.println("New section number: ");
+                int newSection = input2.nextInt();
+                course.setCourseSection(newSection);
+                break;
+            }
+            if(choice == 4 && courseId.equals(course.getCourseId())) {
+                System.out.println("New course location: ");
+                String newLocation = input2.nextLine();
+                course.setCourseLocation(newLocation);
+                break;
+            }
+        }
+        System.out.println(crsMain.courseArrayList.toString());
     }
 
-    public static void display() throws IOException {
+    public static void displayInfo() throws IOException {
         Course course = new Course();
         Scanner input = new Scanner(System.in);
 
@@ -77,7 +113,12 @@ public class Admin extends User {
         System.out.print("Enter course Id: ");
         String courseId = input.nextLine();
 
-
+        for(int i = 0; i < crsMain.courseArrayList.size(); i++) {
+            course = crsMain.courseArrayList.get(i);
+            if(course.getCourseId().equals(courseId)) {
+                System.out.println(crsMain.courseArrayList.get(i));
+            }
+        }
     }
 
     public static void registerStudent() {
@@ -89,22 +130,55 @@ public class Admin extends User {
         String userName = null;
         String password = null;
 
-
+        Student newStudent = new Student(firstName, lastName, userName, password, crsData.courses);
+        crsData.students.add(newStudent);
+        System.out.println(crsData.students.toString());
     }
 
     public static void viewCourses() {
-
+        System.out.println(crsMain.courseArrayList.toString());
     }
 
     public static String fullCourseId = null;
     public static String viewFullCourses() throws IOException {
         Course course = new Course();
-
+        for(int i = 0; i < crsMain.courseArrayList.size(); i++) {
+            course = crsMain.courseArrayList.get(i);
+            if(course.getEnrolledStudents() == course.getMaxStudents()) {
+                String courseId = course.getCourseId();
+                fullCourseId = courseId;
+                System.out.println(courseId);
+            }
+        }
+        return fullCourseId;
     }
 
     public static void addCourseToFile() throws IOException {
         Course course = new Course();
+        try (PrintWriter writer = new PrintWriter(new File("fullCourses.csv"))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int i = 0; i < crsMain.courseArrayList.size(); i++) {
+                course = crsMain.courseArrayList.get(i);
+                if(course.getEnrolledStudents() == course.getMaxStudents()) {
+                    String courseId = course.getCourseId();
+                    stringBuilder.append(courseId);
+                }
+            }
+        }
+    }
 
+    public static void studentNamesInCourse() {
+        Course course = new Course();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter course Id: ");
+        String courseId = input.nextLine();
+
+        for(int i = 0; i < crsMain.courseArrayList.size(); i++) {
+            course = crsMain.courseArrayList.get(i);
+            if(course.getCourseId().equals(courseId)) {
+                System.out.println(course.getStudents().toString());
+            }
+        }
     }
 
     public static void coursesPerStudent() {
@@ -119,13 +193,21 @@ public class Admin extends User {
         Student testStudent = new Student(firstName, lastName, null, null);
         System.out.println("Their registered courses: ");
 
-
+        for(int i = 0; i < crsMain.courseArrayList.size(); i++) {
+            course = crsMain.courseArrayList.get(i);
+            System.out.println(crsData.courses.toString());
+        }
     }
 
     public static void sortMostStudentsPerClass() {
         Course course = new Course();
         ArrayList<Integer> sortedArrayList = new ArrayList<Integer>();
-
+        for(int i = 0; i < crsMain.courseArrayList.size(); i++) {
+            sortedArrayList.add(course.enrolledStudents);
+            String courseId = course.getCourseId();
+        }
+        Collections.sort(sortedArrayList, Collections.reverseOrder());
+        System.out.println(sortedArrayList.toString());
     }
 
 }
