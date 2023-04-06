1ae53e6cb83127613edaf801004e3ae2dd990b4a
TrevisGreen <Trevis.Green@gmail.com>
2023-04-06 07:23:14 -0500

    Added student deserialization method

--- src/Serialization.java
+++ src/Serialization.java
@@ -1,4 +1,24 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public abstract class Serialization implements Serializable {
+    public static void studentSerialization() {
+        ArrayList<Student> serializedStudent = crsData.students;
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("StudentList.ser");
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(serializedStudent);
+            objectOutputStream.close();
+            fileOutputStream.close();
+            System.out.println("Student Serialization Complete");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
