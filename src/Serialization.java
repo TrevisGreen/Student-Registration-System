652e33736a4d833e9838bd2472a2f9c3afebba58
TrevisGreen <Trevis.Green@gmail.com>
2023-04-06 07:33:49 -0500

    Added student deserialization method

--- src/Serialization.java
+++ src/Serialization.java
@@ -1,7 +1,4 @@
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 import java.util.ArrayList;
 
 public abstract class Serialization implements Serializable {
@@ -20,5 +17,29 @@ public abstract class Serialization implements Serializable {
         }
     }
 
+    public static void studentDeserialization() {
+        ArrayList<Student> deserializedStudent = new ArrayList<Student>();
 
+        try {
+            // File input system receives bytes from a file
+            FileOutputStream fileOutputStream = new FileOutputStream("StudentList.ser");
+
+            // ObjectInputStream does the deserialization it reconstructs the data into an object
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileOutputStream);
+
+            // Cast as Student, readObject will take the object from ObjectInputStream
+            deserializedStudent = (ArrayList<Student>) objectInputStream.readObject();
+            objectInputStream.close();
+            fileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        } catch (ClassNotFoundException cnfe) {
+            cnfe.printStackTrace();
+            return;
+        }
+        for(Student studentList : deserializedStudent) {
+            System.out.println(studentList);
+        }
+    }
 }
